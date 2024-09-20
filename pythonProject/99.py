126b303a4dc79766b2d2d7a737ec479566329b0c
Leivmox <leivmox@gmail.com>
2024-09-20 17:16:28 +0800

    python update

--- pythonProject/99.py
+++ pythonProject/99.py
@@ -1,9 +1,8 @@
-print("输入一个整数:")
-a = input()
-
-a2 = int(a)
-for i in range(1, a2+1):
-
+# print("输入一个整数:")
+# a = input()
+#
+# a2 = int(a)
+for i in range(1, 10):
     for j in range(1, i+1):
         print(f"{j}x{i}={i*j}", end="\t")
     print()
